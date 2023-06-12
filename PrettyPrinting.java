515bdf65a7c845fbc4e04dea5bf052bd9558d457
pratyushdev-codes <birole.pratyush@gmail.com>
2023-06-12 14:07:23 +0530

    Pretty Printing

---
+++ PrettyPrinting.java
@@ -0,0 +1,7 @@
+import java.util.*;
+public class PrettyPrinting{
+    public static void main(String[] args) {
+        float a= 789.983828101010;
+        System.out.println("Formatted number is %.2f",a);
+    }
+}
--- sample.java
+++ sample.java
@@ -1,8 +1,6 @@
 import java.util.*;
 public class sample{
     public static void main(String[] args) {
-        String a= new String("Pratyush");
-        String b= new String("Birole");
-        System.out.println(a+b);
+        System.out.println(56);
     }
 }
